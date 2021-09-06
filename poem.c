0fa3be6e5eb542a5f0aeccbe35891e4e7c394b79
Jackie <85565285+missjackieann@users.noreply.github.com>
2021-09-05 20:40:18 -0400

    Add files via upload

---
+++ poem.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    printf("The itsy bitsy spider\nClimbed up the waterspout.\nDown came the rain\nAnd washed the spider out.\nOut came the sun\nAnd dried up all the rain.\nSo the itsy-bitsy spider\nClimbed up the spout again!");
+   
+    return 0;
+}
+
+
+
